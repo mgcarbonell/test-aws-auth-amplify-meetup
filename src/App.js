96674533416cc69634247ea209878b99f3c279dc
mgcarbonell <mgcarbonell@gmail.com>
2021-05-29 15:32:36 -0700

    Created an API and Auth with AWS Amplify-CLI and React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { API, Auth } from 'aws-amplify';
 
 function App() {
+  async function callApi() {
+    const user = await Auth.currentAuthenticatedUser();
+    console.log('user: ', user);
+    const token = user.signInUserSession.idToken.jwtToken;
+    console.log('token: ', token)
+
+    const requestInfo = {
+      headers: {
+        Authorization: token,
+      }
+    }
+
+    const data = await API.get('meetuptestapi', '/hello', requestInfo)
+    console.log("data: ", data);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AmplifyAuthenticator>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+                </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+                </a>
+        </header>
+        <buttion onClick={callApi}>Call API</buttion>
+        <AmplifySignOut />
+      </div>
+    </AmplifyAuthenticator>
+  )
 }
 
-export default App;
+export default withAuthenticator(App);
